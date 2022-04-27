f76b8aa04dad6b6620471a3f511baeb522499531
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-04-27 13:09:11 +0300

    41-v2

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -5,15 +5,18 @@ const SEND_MESSAGE = 'SEND_MESSAGE'
 
 const dialogsReducer = (state, action) => {
 
-    if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        state.newMessageBody = action.body
-    } else if (action.type === SEND_MESSAGE) {
-        let body = state.newMessageBody
-        state.newMessageBody = ''
-        state.message.push({id: 6, message: body})
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body
+            return state
+        case SEND_MESSAGE:
+            let body = state.newMessageBody
+            state.newMessageBody = ''
+            state.message.push({id: 6, message: body})
+            return state
+        default:
+            return state
     }
-
-    return state
 }
 
 export default dialogsReducer;
