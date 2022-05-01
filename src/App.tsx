8a16027db07cf72f9ad985f78cf6357ba3be0fea
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-05-01 16:51:17 +0300

    44: Context API

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
-import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {ReduxStoreType} from "./redux/redux-store";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 
 export type AppPropsType = {
     store: ReduxStoreType
 }
 
 const App: React.FC<AppPropsType> = (props) => {
-    const state = props.store.getState();
+    //const state = props.store.getState();
     return (
         <BrowserRouter>
             <div className="app-wrapper">
@@ -21,10 +21,8 @@ const App: React.FC<AppPropsType> = (props) => {
                 <div className='app-wrapper-content'>
                     <Routes>
                         <Route path="dialogs/*"
-                               element={<DialogsContainer store={props.store}
-                               />}/>
-                        <Route path="profile"
-                               element={<Profile store={props.store}/>}/>
+                               element={<DialogsContainer />}/>
+                        <Route path="profile" element={<Profile />}/>
                         <Route path="news" element={'News'}/>
                     </Routes>
                 </div>
